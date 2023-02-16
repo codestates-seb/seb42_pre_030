526499988a10301dee980b6b3b1ce87cad8ab023
jiilEun <dmswldlf2010@gmail.com>
2023-02-16 11:47:49 +0900

    Init: 백엔드 초기 설정

---
+++ back/viva-stack-overflow/src/main/java/com/vivarepublica/vivastackoverflow/VivaStackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.vivarepublica.vivastackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaStackOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaStackOverflowApplication.class, args);
+	}
+
+}
---
+++ back/viva-stack-overflow/src/test/java/com/vivarepublica/vivastackoverflow/VivaStackOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivarepublica.vivastackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaStackOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
