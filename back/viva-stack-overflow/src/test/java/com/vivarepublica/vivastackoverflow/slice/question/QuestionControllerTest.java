104ba278ec2bd4e43772ef5302e0403c3c569f04
RYE415 <rhk415@gmail.com>
2023-02-24 11:18:45 +0900

    Fix: jwt 오류 수정

--- back/viva-stack-overflow/src/test/java/com/vivarepublica/vivastackoverflow/slice/question/QuestionControllerTest.java
+++ back/viva-stack-overflow/src/test/java/com/vivarepublica/vivastackoverflow/slice/question/QuestionControllerTest.java
@@ -7,6 +7,7 @@ import com.vivarepublica.vivastackoverflow.domain.question.dto.QuestionDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
@@ -38,6 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(QuestionController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureRestDocs
+@AutoConfigureMockMvc(addFilters = false)
 public class QuestionControllerTest {
 
     @Autowired
