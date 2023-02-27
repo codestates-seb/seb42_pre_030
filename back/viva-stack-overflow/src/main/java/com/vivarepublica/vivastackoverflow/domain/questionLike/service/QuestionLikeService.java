b947c4d1fa93ecadcb4c46360fdd2cfab72a5c76
RYE415 <rhk415@gmail.com>
2023-02-28 05:34:52 +0900

    Test: 테스트케이스 Mockito 적용 및 구현

--- back/viva-stack-overflow/src/main/java/com/vivarepublica/vivastackoverflow/domain/questionLike/service/QuestionLikeService.java
+++ back/viva-stack-overflow/src/main/java/com/vivarepublica/vivastackoverflow/domain/questionLike/service/QuestionLikeService.java
@@ -1,5 +1,12 @@
 package com.vivarepublica.vivastackoverflow.domain.questionLike.service;
 
+import com.vivarepublica.vivastackoverflow.domain.member.entity.Member;
+import com.vivarepublica.vivastackoverflow.domain.member.repository.MemberRepository;
+import com.vivarepublica.vivastackoverflow.domain.question.entity.Question;
+import com.vivarepublica.vivastackoverflow.domain.question.repository.QuestionRepository;
+import com.vivarepublica.vivastackoverflow.domain.questionLike.dto.QuestionLikeDto;
+import com.vivarepublica.vivastackoverflow.domain.questionLike.entity.QuestionLike;
+import com.vivarepublica.vivastackoverflow.domain.questionLike.repository.QuestionLikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,30 +16,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class QuestionLikeService {
 
-//    private final QuestionLikeRepository questionLikeRepository;
-//    private final MemberRepository memberRepository;
-//    private final QuestionRepository questionRepository;
-//
-//    public void post(QuestionLikeDto questionLikeDto) throws Exception {
-//
-//        Member member = memberRepository.findById(questionLikeDto.getMemberId())
-//                .orElseThrow(() -> new RuntimeException("회원 정보를 찾을 수 없습니다."));
-//
-//        Question question = questionRepository.findById(questionLikeDto.getQuestionId())
-//                .orElseThrow(() -> new RuntimeException("질문을 찾을 수 없습니다."));
-//
-//        //좋아요 이미 존재할 경우 에러 리턴
-//        if (questionLikeRepository.findByMemberAndQuestion(member, question).isPresent()) {
-//            // error
-//            throw new RuntimeException("이미 처리된 좋아요입니다.");
-//        }
-//
-//        QuestionLike questionLike = QuestionLike.builder()
-//                                                .question(question)
-//                                                .member(member)
-//                                                .build();
-//
-//        questionLikeRepository.save(questionLike);
-//        questionRepository.addLikeCount(question);
-//    }
+    private final QuestionLikeRepository questionLikeRepository;
+    private final MemberRepository memberRepository;
+    private final QuestionRepository questionRepository;
+
+    public QuestionLikeDto post(QuestionLikeDto questionLikeDto) throws Exception {
+
+        Member member = memberRepository.findById(questionLikeDto.getMemberId())
+                .orElseThrow(() -> new RuntimeException("회원 정보를 찾을 수 없습니다."));
+
+        Question question = questionRepository.findById(questionLikeDto.getQuestionId())
+                .orElseThrow(() -> new RuntimeException("질문을 찾을 수 없습니다."));
+
+        //좋아요 이미 존재할 경우 에러 리턴
+        if (questionLikeRepository.findByMemberAndQuestion(member, question).isPresent()) {
+            // error
+            throw new RuntimeException("이미 처리된 좋아요입니다.");
+        }
+
+        QuestionLike questionLike = QuestionLike.builder()
+                                                .question(question)
+                                                .member(member)
+                                                .build();
+
+        questionLikeRepository.save(questionLike);
+        questionRepository.addLikeCount(question);
+        return questionLikeDto;
+    }
+
 }
